edd551a11a1e462bd30547550c1385f7b8478da9
Mbah Neilvani <neilvanimbah@gmail.com>
2024-02-17 12:18:24 +0100

    stated the other off-screen-section

--- Js/main.js
+++ Js/main.js
@@ -3,18 +3,15 @@ const menu = document.querySelector('#menu');
 const hamMenu = document.querySelector('.ham-menu')
 const hamburger = document.querySelector('#hamburger')
 const offScreenMenu = document.querySelector('#off-screen-menu-a');
-// const downbtn = document.querySelector('.down')
-
-
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active'); 
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
-    // downbtn.classList.toggle('active')
 })
 
-// hammenu.addEventListener('click', ()=> {
-//     alert('working')
-//     hammenu.classList.toggle('active')
-// })
+hamMenu.addEventListener('click', ()=> {
+    menu.classList.toggle('active')
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
