81ff7d264724fc154d978fb52727b074dd6cfd11
Mbah Neilvani <neilvanimbah@gmail.com>
2024-09-24 12:07:22 +0100

    still updating the off-screen

--- Js/main.js
+++ Js/main.js
@@ -1,18 +1,18 @@
-const btn = document.querySelector('.btn-a');
-// const hammenu = document.querySelector('.burger')
-const things = document.querySelector('#thing');
-const downbtn = document.querySelector('.down')
-const offScreenMenu = document.querySelector('#off-screen-menu-a');
+// const btn = document.querySelector('.btn-a');
+// // const hammenu = document.querySelector('.burger')
+// const things = document.querySelector('#thing');
+// const downbtn = document.querySelector('.down')
+// const offScreenMenu = document.querySelector('#off-screen-menu-a');
 
 
-btn.addEventListener('click', () => {
-    btn.classList.toggle('active');
-    offScreenMenu.classList.toggle('active'); 
-    things.classList.toggle('active');
-    downbtn.classList.toggle('active')
-})
+// btn.addEventListener('click', () => {
+//     btn.classList.toggle('active');
+//     offScreenMenu.classList.toggle('active'); 
+//     things.classList.toggle('active');
+//     downbtn.classList.toggle('active')
+// })
 
-hammenu.addEventListener('click', ()=> {
-    alert('working')
-    hammenu.classList.toggle('active')
-})
+// hammenu.addEventListener('click', ()=> {
+//     alert('working')
+//     hammenu.classList.toggle('active')
+// })
