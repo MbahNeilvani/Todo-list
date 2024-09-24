6119b122775d40bf2340f86bf83c3e448fddf1f5
Mbah Neilvani <neilvanimbah@gmail.com>
2024-09-24 11:03:58 +0100

    started the Css all over

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.btn-a');
 // const hammenu = document.querySelector('.burger')
 const things = document.querySelector('#thing');
+const downbtn = document.querySelector('.down')
 const offScreenMenu = document.querySelector('#off-screen-menu-a');
 
 
@@ -8,6 +9,7 @@ btn.addEventListener('click', () => {
     btn.classList.toggle('active');
     offScreenMenu.classList.toggle('active'); 
     things.classList.toggle('active');
+    downbtn.classList.toggle('active')
 })
 
 hammenu.addEventListener('click', ()=> {
