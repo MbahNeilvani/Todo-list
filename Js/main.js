c23cccadd75a0e63df3f25092e820675222095b4
Mbah Neilvani <neilvanimbah@gmail.com>
2024-09-25 11:58:47 +0100

    finished the 1st off-screen-section

--- Js/main.js
+++ Js/main.js
@@ -1,18 +1,19 @@
-// const btn = document.querySelector('.btn-a');
-// // const hammenu = document.querySelector('.burger')
-// const things = document.querySelector('#thing');
+const btn = document.querySelector('.btn-a');
+// const hammenu = document.querySelector('.burger')
+const offScreenMenu = document.querySelector('#off-screen-menu-a');
 // const downbtn = document.querySelector('.down')
-// const offScreenMenu = document.querySelector('#off-screen-menu-a');
+const menu = document.querySelector('#menu');
 
 
-// btn.addEventListener('click', () => {
-//     btn.classList.toggle('active');
-//     offScreenMenu.classList.toggle('active'); 
-//     things.classList.toggle('active');
-//     downbtn.classList.toggle('active')
-// })
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active'); 
+    // alert('working')
+    // btn.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+    // downbtn.classList.toggle('active')
+})
 
-// hammenu.addEventListener('click', ()=> {
-//     alert('working')
-//     hammenu.classList.toggle('active')
-// })
+hammenu.addEventListener('click', ()=> {
+    alert('working')
+    hammenu.classList.toggle('active')
+})
