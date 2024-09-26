e60810739b0eb2b986c7de703a2f5720d39d1117
Mbah Neilvani <neilvanimbah@gmail.com>
2024-09-26 10:00:37 +0100

    done with the 1st off-screen section

--- Js/main.js
+++ Js/main.js
@@ -1,19 +1,20 @@
 const btn = document.querySelector('.btn-a');
-// const hammenu = document.querySelector('.burger')
+const menu = document.querySelector('#menu');
+const hamMenu = document.querySelector('.ham-menu')
+const hamburger = document.querySelector('#hamburger')
 const offScreenMenu = document.querySelector('#off-screen-menu-a');
 // const downbtn = document.querySelector('.down')
-const menu = document.querySelector('#menu');
+
 
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active'); 
-    // alert('working')
-    // btn.classList.toggle('active');
+    hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
     // downbtn.classList.toggle('active')
 })
 
-hammenu.addEventListener('click', ()=> {
-    alert('working')
-    hammenu.classList.toggle('active')
-})
+// hammenu.addEventListener('click', ()=> {
+//     alert('working')
+//     hammenu.classList.toggle('active')
+// })
