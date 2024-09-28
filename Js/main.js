ddb7459e8882a2b9a15bd169f5fb6a2929d75483
Mbah Neilvani <neilvanimbah@gmail.com>
2024-09-28 21:51:46 +0100

    corrected some stuffs

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector('.btn-a');
+const btnB = document.querySelector('.btn-b');
 const menu = document.querySelector('#menu');
+const status = document.querySelector('.status');
 const hamMenu = document.querySelector('.ham-menu')
 const hamburger = document.querySelector('#hamburger')
 const offScreenMenu = document.querySelector('#off-screen-menu-a');
@@ -14,4 +16,8 @@ hamMenu.addEventListener('click', ()=> {
     menu.classList.toggle('active')
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+})
+
+btnB.addEventListener('click', () =>{
+    status.classList.toggle('active')
 })
