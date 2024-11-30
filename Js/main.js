963af7106b03cfbeaad8ebe07cf9183f8c69b7a9
Mbah Neilvani <neilvanimbah@gmail.com>
2024-11-30 11:42:20 +0100

    updated the css file

--- Js/main.js
+++ Js/main.js
@@ -21,3 +21,17 @@ hamMenu.addEventListener('click', ()=> {
 btnB.addEventListener('click', () =>{
     status.classList.toggle('active')
 })
+
+document.querySelector('#project').innerText = localStorage.getItem('message')
+
+if (!localStorage.getItem('message')){
+    localStorage.getItem('message', 0)
+}
+
+document.querySelector('button').addEventListener('click', addAnotherOne)
+
+function addAnotherOne(){
+   let botScoreVal = Number(localStorage.getItem('botScore') )
+   botScoreVal += 1
+   localStorage.setItem('botScore', botScoreVal)
+}
